f91541a2f02e5571f20213c51ce958f749f2b018
Dylan Piercey <dpiercey@ebay.com>
2022-10-24 11:07:03 -0700

    feat: add devtools plugin by default
    BREAKING CHANGE: Make graphql a peerDependency

--- src/client/index.ts
+++ src/client/index.ts
@@ -7,25 +7,34 @@ import {
 } from "@urql/core";
 import lookup from "./lookup";
 
+type Fetch = typeof fetch;
+
 export function createClient({
   isServer,
   fetch,
 }: {
   isServer: boolean;
-  fetch?: (resource: RequestInfo, options?: RequestInit) => Promise<Response>;
+  fetch?: Fetch;
 }) {
   // The `ssrExchange` must be initialized with `isClient` and `initialState`
-  const ssr = ssrExchange({
-    isClient: !isServer,
-  });
+  const ssr = ssrExchange({ isClient: !isServer });
+  let exchanges = [
+    dedupExchange,
+    cacheExchange,
+    ssr, // Add `ssr` in front of the `fetchExchange`
+    fetchExchange,
+  ];
+
+  if (typeof window === "object" && process.env.NODE_ENV !== "production") {
+    exchanges = [
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require("@urql/devtools").devtoolsExchange,
+      ...exchanges,
+    ];
+  }
 
   const client = internalCreate({
-    exchanges: [
-      dedupExchange,
-      cacheExchange,
-      ssr, // Add `ssr` in front of the `fetchExchange`
-      fetchExchange,
-    ],
+    exchanges,
     fetch,
     ...lookup.config,
   });
